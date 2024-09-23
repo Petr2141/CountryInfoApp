0e6f9f5d77962001bd5fb53783a057208067e46b
petr <psulilo@mentalstack.com>
2024-09-22 22:26:13 -0700

    WIP Template for manual di

---
+++ app/src/main/java/com/example/countryinfoapp/CountriesApplication.kt
@@ -0,0 +1,11 @@
+package com.example.countryinfoapp
+
+import android.app.Application
+
+class CountriesApplication : Application() {
+//    lateinit var container: AppContainer
+//    override fun onCreate() {
+//        super.onCreate()
+//        container = DefaultAppContainer()
+//    }
+}
