a580d7d66e90b0f17a66d3cac25c7c2c3c60d12b
petr <psulilo@mentalstack.com>
2024-09-22 22:22:01 -0700

    Added useCase

--- app/src/main/java/com/example/countryinfoapp/domain/GetSortCountriesUseCase.kt
+++ app/src/main/java/com/example/countryinfoapp/domain/GetSortCountriesUseCase.kt
@@ -1,20 +1,20 @@
 package com.example.countryinfoapp.domain
 
+import com.example.countryinfoapp.data.network.model.NetworkCountries
 import com.example.countryinfoapp.data.repository.CountriesRepository
-import com.example.countryinfoapp.model.Country
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class GetSortCountriesUseCase constructor(
     private val countriesRepository: CountriesRepository
 ) {
-    operator fun invoke(sortBy: CountriesSortField = CountriesSortField.CountriesName): Flow<List<Country>> =
-        countriesRepository.getCountries().map { countries ->
+    operator fun invoke(sortBy: CountriesSortField = CountriesSortField.CountriesName): Flow<List<NetworkCountries>> =
+        countriesRepository.countries.map { countries ->
             when (sortBy) {
                 CountriesSortField.Capital       -> countries.sortedBy { it.capital }
                 CountriesSortField.CountriesName -> countries.sortedBy { it.name }
-                CountriesSortField.CurrencyName  -> countries.sortedBy { it.currency }
-                CountriesSortField.LanguageName  -> countries.sortedBy { it.language }
+                CountriesSortField.CurrencyName  -> countries.sortedBy { it.currency.name }
+                CountriesSortField.LanguageName  -> countries.sortedBy { it.language.name }
             }
         }
 }
