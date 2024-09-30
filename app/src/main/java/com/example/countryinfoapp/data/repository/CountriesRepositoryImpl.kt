b29c3e670030963de1e761cf6189fb04a13084f5
petr <psulilo@mentalstack.com>
2024-09-29 23:53:25 -0700

    Call mapper

--- app/src/main/java/com/example/countryinfoapp/data/repository/CountriesRepositoryImpl.kt
+++ app/src/main/java/com/example/countryinfoapp/data/repository/CountriesRepositoryImpl.kt
@@ -2,16 +2,12 @@ package com.example.countryinfoapp.data.repository
 
 import com.example.countryinfoapp.data.adapter.CountryDataModelInterface
 import com.example.countryinfoapp.data.adapter.NetworkCountryDataAdapter
+import com.example.countryinfoapp.data.adapter.asCountryEntity
 import com.example.countryinfoapp.data.local.database.CountriesDataBaseDataSource
 import com.example.countryinfoapp.data.network.CountriesNetworkDataSource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flow
-
-//TODO Если у нас в ui есть diffUtil, то обновляться будут только ячейки с изменениями
-//Тогда получается если мы отобразим из БД а потом этоже из сетки, то UI не моргнет
-//А если данных в бд нет, будем ждать в ui
-//Если сетка отвалилась, то покажем из бд без апдейта из сети
 
 class CountriesRepositoryImpl(
     private val networkDataSource: CountriesNetworkDataSource,
@@ -23,7 +19,8 @@ class CountriesRepositoryImpl(
             emit(cachedCountries)
         } else {
             val countriesList = networkDataSource.getCountries()
-            dataBaseDataSource.insertCountries( countriesList.map { NetworkCountryDataAdapter(it) })
+            dataBaseDataSource.insertCountries( countriesList
+                .map { NetworkCountryDataAdapter(it).asCountryEntity() })
             emit( countriesList.map { NetworkCountryDataAdapter(it) })
         }
     }
