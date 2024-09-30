b855f300d9af8ca24f5987fbbc0f0a89635401b2
petr <psulilo@mentalstack.com>
2024-09-29 23:50:57 -0700

    Add DAO

--- app/src/main/java/com/example/countryinfoapp/data/local/database/dao/CountryDao.kt
+++ app/src/main/java/com/example/countryinfoapp/data/local/database/dao/CountryDao.kt
@@ -1,17 +1,19 @@
 package com.example.countryinfoapp.data.local.database.dao
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.example.countryinfoapp.data.adapter.CountryDataModelInterface
+import com.example.countryinfoapp.data.local.database.model.CountryEntity
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface CountryDao {
     @Query("SELECT * FROM countries")
-    fun getAllCountries(): Flow<List<CountryDataModelInterface>>
+    fun getAllCountries(): Flow<List<CountryEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertCountries(countries: List<CountryDataModelInterface>)
+    suspend fun insertCountries(countries: List<CountryEntity>)
 
     @Query("DELETE FROM countries")
     suspend fun deleteAllCountries()
