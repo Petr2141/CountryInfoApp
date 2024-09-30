22757b352ee990d8c3a76c7e3d04326ac8ff5b38
petr <psulilo@mentalstack.com>
2024-09-29 23:49:21 -0700

    Add DAO

--- app/src/main/java/com/example/countryinfoapp/data/local/database/CountriesDataBaseDataSource.kt
+++ app/src/main/java/com/example/countryinfoapp/data/local/database/CountriesDataBaseDataSource.kt
@@ -1,6 +1,8 @@
 package com.example.countryinfoapp.data.local.database
 
+import androidx.room.Dao
 import com.example.countryinfoapp.data.local.database.dao.CountryDao
 
+@Dao
 interface CountriesDataBaseDataSource : CountryDao {
 }
