24424398ff629bbf2abf7b086ad7ebb0da659f70
petr <psulilo@mentalstack.com>
2024-09-29 23:53:49 -0700

    Used CountryDataModelInterface

--- app/src/main/java/com/example/countryinfoapp/data/local/database/model/CountryEntity.kt
+++ app/src/main/java/com/example/countryinfoapp/data/local/database/model/CountryEntity.kt
@@ -2,12 +2,13 @@ package com.example.countryinfoapp.data.local.database.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.countryinfoapp.data.adapter.CountryDataModelInterface
 
 @Entity(tableName = "countries")
-data class CountryEntity (
+data class CountryEntity(
     @PrimaryKey
-    val code: String,
-    val name: String,
-    val region: String,
-    val capital: String
-)
+    override val code: String,
+    override val name: String,
+    override val region: String,
+    override val capital: String
+) : CountryDataModelInterface
